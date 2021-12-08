a6e79006f0543f5c2790f1fe8fb26f86f6135072
Francisco García <francoffee57@gmail.com>
2021-12-07 22:25:24 -0600

    Diseño y funcionalidad basica terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const imgContainer = document.getElementById("images")
+const createButton = document.getElementById("create")
+const removeButton = document.getElementById("remove")
+const accountantContainer =  document.getElementById("accountant")
+let i = 0
 
-console.log('Happy hacking :)')
+createButton.onclick = () => crateRandomImage()
+removeButton.onclick = () => removeImage()
+
+ 
+const random = (minimum = 1, maximum = 123) => {
+    return Math.floor(Math.random() * (maximum - minimum)) + minimum
+}
+
+
+const crateRandomImage = () => {   
+    let img = document.createElement('img')
+    img.src = `https://randomfox.ca/images/${random()}.jpg`
+
+    img.className ="img-random"
+
+    imgContainer.appendChild(img)
+
+    accountant(1)
+}
+
+const removeImage = () => {
+    const imgToRemove = document.querySelectorAll("#images img")[0]
+    if(imgToRemove !== undefined){
+        imgContainer.removeChild(imgToRemove)
+    }
+    accountant(-1)
+}
+
+
+const accountant = (num) => {
+    if(i >= 0){
+        i += num
+        console.log(i + num);
+        
+    }else{
+        i = 0
+    }
+    accountantContainer.innerText = i
+}
